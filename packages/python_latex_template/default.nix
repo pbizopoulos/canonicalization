bd5c0558f9f8b0a7aad69de4887f432eecccaf62
Paschalis Bizopoulos <pbizop@gmail.com>
2026-05-05 15:28:34 +0300

    .

--- packages/python_latex_template/default.nix
+++ packages/python_latex_template/default.nix
@@ -17,19 +17,9 @@ let
   '';
 in
 pkgs.python313Packages.buildPythonPackage rec {
-  doInstallCheck = pkgs.stdenv.isLinux;
-  installCheckPhase = ''
-    runHook preInstallCheck
-    "$out/bin/${pname}"
-    test -f "$PWD/tmp/ms.pdf"
-    test -s "$PWD/tmp/ms.pdf"
-    HOME="$(mktemp -d)" coverage erase
-    HOME="$(mktemp -d)" DEBUG=1 coverage run --source="$src" "$src/main.py"
-    HOME="$(mktemp -d)" coverage run --append --source="$src" "$src/main.py"
-    coverage report --fail-under=100
-    HOME="$(mktemp -d)" DEBUG=1 PYTHONWARNINGS=error pyinstrument "$src/main.py"
-    runHook postInstallCheck
-  '';
+  doInstallCheck = false;
+  dontBuild = true;
+  dontUsePythonImportsCheck = true;
   installPhase = ''
     install -Dm644 ./main.py $out/bin/main.py
     install -Dm644 ./ms.tex $out/bin/ms.tex
@@ -37,13 +27,10 @@ pkgs.python313Packages.buildPythonPackage rec {
     install -Dm755 ${runtimeScript} $out/bin/${pname}
   '';
   meta.mainProgram = pname;
-  nativeInstallCheckInputs = [
-    pkgs.python313Packages.coverage
-    pkgs.python313Packages.pyinstrument
+  pname = builtins.baseNameOf src;
+  propagatedBuildInputs = pythonDeps ++ [
     pkgs.texliveFull
   ];
-  pname = builtins.baseNameOf src;
-  propagatedBuildInputs = pythonDeps;
   pyproject = false;
   src = ./.;
   version = "0.0.0";
