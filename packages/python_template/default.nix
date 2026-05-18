2a1c389a4de99c9453dc88c677bb14368448e8f6
Paschalis Bizopoulos <pbizop@gmail.com>
2026-05-18 17:25:46 +0300

    .

--- packages/python_template/default.nix
+++ packages/python_template/default.nix
@@ -1,24 +1,36 @@
 {
+  inputs,
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.python3Packages.buildPythonPackage rec {
+let
+  installationScript = inputs.agenix-shell.lib.installationScript pkgs.stdenv.system {
+    secrets.secrets.file = ../../secrets/secrets.age;
+  };
+  pyPkgs = pkgs.python312Packages;
+  python = pkgs.python312;
+in
+pyPkgs.buildPythonPackage rec {
   installCheckPhase = ''
     runHook preInstallCheck
-    test -x "$out/bin/${pname}"
     HOME="$(mktemp -d)" DEBUG=1 PYTHONWARNINGS=error coverage run --source="$src" -m pyinstrument "$src/main.py"
     coverage report
     runHook postInstallCheck
   '';
   installPhase = ''
+    install -Dm644 main.py $out/${python.sitePackages}/${pname}.py
     install -Dm755 ./main.py $out/bin/${pname}
   '';
   meta.mainProgram = pname;
   nativeInstallCheckInputs = [
-    pkgs.python3Packages.coverage
-    pkgs.python3Packages.pyinstrument
+    pyPkgs.coverage
+    pyPkgs.pyinstrument
   ];
   pname = baseNameOf ./.;
   pyproject = false;
+  shellHook = ''
+    source ${pkgs.lib.getExe installationScript}
+    export $secrets
+  '';
   src = ./.;
   strictDeps = true;
   version = "0.0.0";
