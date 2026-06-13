8cb6fe1af9c91891f95189fb2bbbcacaaa337b69
Paschalis Bizopoulos <pbizop@gmail.com>
2026-06-13 11:55:40 +0300

    .

--- checks/c_template/default.nix
+++ checks/c_template/default.nix
@@ -3,29 +3,18 @@
   pkgs,
   ...
 }:
-pkgs.testers.runNixOSTest rec {
+let
   name = builtins.baseNameOf ./.;
-  nodes.machine =
-    {
-      pkgs,
-      ...
-    }:
-    let
-      cTemplateForCheck = inputs.self.packages.${pkgs.stdenv.system}.${name}.overrideAttrs (_: {
-        NIX_CFLAGS_COMPILE = "-O1 -g3 -fno-omit-frame-pointer -fno-sanitize-recover=all -fsanitize=address,undefined";
-      });
-    in
-    {
-      environment = {
-        systemPackages = [
-          cTemplateForCheck
-        ];
-        variables = {
-          ASAN_OPTIONS = "abort_on_error=1:strict_string_checks=1:detect_stack_use_after_return=1:check_printf=1:allocator_may_return_null=0";
-          UBSAN_OPTIONS = "halt_on_error=1:print_stacktrace=1:report_error_type=1";
-        };
-      };
-    };
+  packageDrv = inputs.self.packages.${pkgs.stdenv.system}.${name};
+in
+pkgs.testers.runNixOSTest {
+  inherit name;
+  nodes.machine = _: {
+    environment.systemPackages = [
+      packageDrv
+    ]
+    ++ (packageDrv.runtimeInputs or [ ]);
+  };
   testScript = ''
     machine.succeed("${name}")
   '';
