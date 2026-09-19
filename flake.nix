5c46af1ea1988b5ed2c4abb097bdee72a0f608b5
Paschalis Bizopoulos <pbizop@gmail.com>
2026-09-19 11:25:02 +0300

    .

--- flake.nix
+++ flake.nix
@@ -4,12 +4,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:numtide/blueprint";
     };
-    disko = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/disko";
-    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    preservation.url = "github:nix-community/preservation";
     treefmt-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:numtide/treefmt-nix";
